14a64d3e22b1fe2fc50073271f203d3cc49c3bbe
alumne1daw <alumne1daw@localhost>
2015-12-22 17:10:30 +0100

    Commit:

--- NadalAscii/src/main/java/net/javierjimenez/App.java
+++ NadalAscii/src/main/java/net/javierjimenez/App.java
@@ -5,32 +5,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
- * Hello world!
+ * 
+ * @author alumne1daw
  *
  */
 public class App {
 
-	private static String NOMBRE_FICHERO = "/visitas.txt";
-	
+	/**
+	 * Variable que ens enllaça a l'arxiu de text a llegir.
+	 */
+	private static String NOM_FITXER = "/visites.txt";
+
+	/**
+	 * Metode principal del programa, on llegirem l'arxiu de text
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		
+
 		BufferedReader br = null;
 		Comprobar casas;
 
+		/**
+		 * Creem un bloq try/catch/finally per capturar les excepcions a l'hora
+		 * de llegir el fitxer.
+		 */
 		try {
 
 			String casa;
 
-			br = new BufferedReader(new InputStreamReader(App.class.getResourceAsStream(NOMBRE_FICHERO)));
+			br = new BufferedReader(new InputStreamReader(App.class.getResource(NOM_FITXER).openStream()));
 
 			while ((casa = br.readLine()) != null) {
 
 				casas = new Comprobar(casa);
-				
+
 				System.out.println(casas.visitasCasas());
 			}
 
 		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} catch (Exception e) {
 
 			e.printStackTrace();
 
--- NadalAscii/src/main/java/net/javierjimenez/Comprobar.java
+++ NadalAscii/src/main/java/net/javierjimenez/Comprobar.java
@@ -1,64 +1,91 @@
 package net.javierjimenez;
 
+/**
+ * 
+ * @author alumne1daw
+ *
+ */
 public class Comprobar {
 
 	private String Noel = "*<]:-DOo";
-	private String Reno = ">:o)";
-	private String Elfo = "<]:-D";
+	private String Ren = ">:o)";
+	private String Elf = "<]:-D";
 	private String casa;
 
+	/**
+	 * Constructor principal de l'objecte Comprobar
+	 * 
+	 * @param c String que conte una linia del fitxer llegit
+	 */
 	public Comprobar(String c) {
 
 		casa = c;
-		
+
 	}
 
+	/**
+	 * Metode per comprobar qui visita cada casa i quantes vegades
+	 * 
+	 * @return String que conté la informació de qui visita 
+	 * i quantes vegades ho fa.
+	 */
 	public String visitasCasas() {
 
-		String quienVisita = "";
+		String quiVisita = "";
 
-		int papaNoel = busquedaNavidad(Noel);
-		int renos = busquedaNavidad(Reno);
-		int elfos = busquedaNavidad(Elfo);
-		
-		if (papaNoel == 0 && renos == 0 && elfos == 0) {
-			
+		int pareNoel = busquedaNavidad(Noel);
+		int rens = busquedaNavidad(Ren);
+		int elfs = busquedaNavidad(Elf);
+
+		if (pareNoel == 0 && rens == 0 && elfs == 0) {
+
 			return "-";
-			
+
 		} else {
 
-			if (papaNoel > 0) {
+			if (pareNoel > 0) {
 
-				quienVisita = "Papa Noel(" + papaNoel + ") ";
-				elfos = elfos - papaNoel;
-
-			}
-			if (renos > 0) {
-
-				quienVisita = quienVisita + "Reno(" + renos + ") ";
+				quiVisita = "Pare Noel(" + pareNoel + ") ";
+				elfs = elfs - pareNoel;
 
 			}
-			if (elfos > 0) {
+			if (rens > 0) {
 
-				quienVisita = quienVisita + "Elfo(" + elfos + ") ";
+				quiVisita = quiVisita + "Ren(" + rens + ") ";
+
+			}
+			if (elfs > 0) {
+
+				quiVisita = quiVisita + "Elf(" + elfs + ") ";
 
 			}
 		}
-		
-		return quienVisita;
+
+		return quiVisita;
 	}
 
+	/**
+	 * Metode per buscar en cada linia del fitxer si hi ha
+	 * alguna de les cadenes Ascii que busquem.
+	 * 
+	 * @param busqueda String que conté la cadena Ascii
+	 * per buscar en el fitxer.
+	 * 
+	 * @return Int que conté el total de vegades que hi ha
+	 * la cadena Ascii dins de la linia llegida del fitxer.
+	 */
 	private int busquedaNavidad(String busqueda) {
-		
+
 		int contador = 0;
 		int posicio = 0;
 		int pos = 0;
-		
-		while(pos >= 0){
+
+		while (pos >= 0) {
 			pos = casa.indexOf(busqueda, posicio);
 			posicio = pos + busqueda.length();
-			
-			if(pos > 0) contador++;
+
+			if (pos > 0)
+				contador++;
 		}
 		return contador;
 	}
